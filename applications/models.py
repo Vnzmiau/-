7116c42f10a09343e05047af447ad26d63395fa4
Vnzmiau <viliyan.balchev@gmail.com>
2024-06-15 19:13:39 +0300

    Created a model:Application
    i created a field for applicant-the person who created it,profession-to type what profesion the aplicant has,what education,work experience,when it was last updated and when it was created

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Application(models.Model):
+    applicant=models.OneToOneField(User,on_delete=models.SET_NULL,null=True)
+    profession=models.CharField(max_length=100)
+    education=models.TextField()
+    work_experience=models.TextField()
+    updated=models.DateTimeField(auto_now=True)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
 
 # Create your models here.
