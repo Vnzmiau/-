c109c805dc3efefc9ff796d422129c472424251d
Vnzmiau <viliyan.balchev@gmail.com>
2024-06-16 17:05:42 +0300

    fixed urls

--- applications/urls.py
+++ applications/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('applications/', views.manageApplications, name='applications_list'),
-    path('applications/<int:application_id>/', views.manageApplications, name='application_detail'),
+    path('applications/<int:application_id>/', views.manageApplication, name='application_detail'),
 ]
