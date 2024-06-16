76e3b3c13268e6adb27c034dfad88c6c76c49943
Vnzmiau <viliyan.balchev@gmail.com>
2024-06-16 16:43:29 +0300

    added in the url routhing

--- applications/urls.py
+++ applications/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
+    path('applications/', views.manageApplications, name='applications_list'),
+    path('applications/<int:application_id>/', views.manageApplications, name='application_detail'),
 ]
