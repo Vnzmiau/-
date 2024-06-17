1fa9a9bbcbecdb884e0ea79ea1798885399287d7
Vnzmiau <viliyan.balchev@gmail.com>
2024-06-17 12:29:53 +0300

    added url routhing for the register,login and logout

--- applications/urls.py
+++ applications/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('applications/', views.manageApplications, name='applications_list'),
     path('applications/<int:application_id>/', views.manageApplication, name='application_detail'),
+    path('login/',views.loginPage,name='login'),
+    path('register/',views.registerPage,name='register'),
+    path('logout/',views.logoutpage,name='logout'),
 ]
